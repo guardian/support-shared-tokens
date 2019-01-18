e0774302ad8922021e8b7ca4f6f596c87d496272
Laura <lawr@lauragonzalez.cc>
2019-01-18 14:09:16 +0000

    generate markup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 
 import Header from './Header';
 
 ReactDOM.render(<Header />, document.getElementById('app'));
+
+document.querySelector('code').innerText = ReactDOMServer.renderToString(
+	<Header />
+);
---
+++ src/scripts/generateHtml.js
@@ -0,0 +1,1 @@
+const { ReactDOMServer } = require('react-dom/server');
